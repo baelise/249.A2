f5d919758022a0a910bdf0165289d0775ae7dcc3
baelise <94066054+baelise@users.noreply.github.com>
2022-02-28 15:02:45 -0500

    Update driver.java

--- driver.java
+++ driver.java
@@ -153,3 +153,76 @@ public class driver {
 
 	}
 }
+
+//DRIVER ii
+// package driverPack;
+// import Airplanes.*;
+// import Helicopters.*;
+// import DroneMAV.*;
+// import Multirotors.*;
+// import UAVs.*;
+
+// public class driver {
+// 	public static Object[] copyFlyingObjects(Object[] o) {
+// 		Object[] newA = new Object[o.length];
+// 		for(int i = 0; i < o.length; i++) {
+// 			newA[i] = o[i];
+// 		}
+// 		return newA;
+// 	}
+
+// 	public static void main(String[] args) {
+		
+		
+// 		Airplane a = new Airplane("TY", 2003333.3, 300000);
+// 		UAV b = new UAV(99.99, 99.99);
+// 		Helicopter c = new Helicopter();
+// 		Airplane d = new Airplane("TY", 2003333.3, 300000);
+// 		MAV e = new MAV();
+// 		Airplane f = new Airplane("TY", 2003333.3, 300000);
+// 		UAV g = new UAV(9.9, 9.9);
+// 		UAV h = new UAV(9999.9999, 9999.9999);
+// 		UAV i = new UAV(1.1, 2.2);
+// 		UAV j = new UAV(3.3, 4.4);
+// 		UAV k = new UAV(5.5, 6.6);
+// 		UAV l = new UAV(7.7, 8.8);
+// 		UAV m = new UAV(9.9, 10.10);
+// 		UAV n = new UAV(11.11, 12.12);
+// 		UAV o = new UAV(13.13, 14.14);
+// 		UAV p = new UAV(15.15, 16.16);
+// 		AgriculturalDrone q = new AgriculturalDrone(4.4, 4.4,"P", 3);
+// 		Quadcopter r = new Quadcopter("", 6.6, 67, 5, 6, 7, 8);
+// 		Multirotor s = new Multirotor("", 6.6, 67, 5, 6, 7, 9);
+// 		Helicopter t = new Helicopter();
+// 		AgriculturalDrone u = new AgriculturalDrone(4.4, 4.4,"P", 3);
+// 		AgriculturalDrone v = new AgriculturalDrone(4.4, 4.4,"P", 3);
+// 		Helicopter w = new Helicopter();
+// 		Airplane x = new Airplane("TY", 2003333.3, 300000);
+// 		Quadcopter y = new Quadcopter("", 6.6, 67, 5, 6, 7, 8);
+// 		Multirotor z = new Multirotor("", 6.6, 67, 5, 6, 7, 9);
+// 		Quadcopter a1 = new Quadcopter("", 6.6, 67, 5, 6, 7, 8);
+// 		Multirotor b1 = new Multirotor("", 6.6, 67, 5, 6, 7, 9);
+		
+// 		//Arrays
+// 		Object[] test = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, a1, b1};
+// 		Object[] test1 = {a, c, d, e, f, q, r, s, t, u, v, w, x, y, z, a1, b1};
+// 		Object[] test2 = {a, b, c, d, e, f, q, r, s, t, u, v, w, x, y, z, a1, b1};
+
+// 		//Copy flying object array
+// 		Object[] newOne = copyFlyingObjects(test);
+// 		System.out.println(newOne.length);
+// 		for(int aa = 0; aa < newOne.length; aa++) {
+// 			System.out.println(newOne[aa].toString());
+// 		}
+// 		Object[] newOne2 = copyFlyingObjects(test1);
+// 		System.out.println(newOne2.length);
+// 		for(int bb= 0; bb < newOne2.length; bb++) {
+// 			System.out.println(newOne2[bb].toString());
+// 		}
+// 		Object[] newOne3 = copyFlyingObjects(test2);
+// 		System.out.println(newOne3.length);
+// 		for(int cc = 0; cc < newOne3.length; cc++) {
+// 			System.out.println(newOne3[cc].toString());
+// 		}
+// 	}
+// }
