a82a6adc9e0efee1f6f24b2037929c92c995dc0f
baelise <94066054+baelise@users.noreply.github.com>
2022-03-12 11:45:53 -0500

    Update Multirotor.java

--- Multirotor.java
+++ Multirotor.java
@@ -1,10 +1,15 @@
+//--------------------------------------------------------------------
+// Elise Proulx (40125538) and Andrei Barbulescu (40208635)
+// COMP 249 - Section S
+// Assignment 2
+// 03/04/2022
+//--------------------------------------------------------------------
 package Multirotors;
-
 import Helicopters.*;
 
 public class Multirotor extends Helicopter {
-	int numRot;
-	//CONSTRUCTORS
+	private int numRot;
+	// Constructors
 	public Multirotor () {
 		super();
 		this.numRot = 0;
@@ -16,26 +21,25 @@ public class Multirotor extends Helicopter {
 		this.cap = m.cap;
 		this.numRot = m.numRot;
 	}
-	
 	public Multirotor (String b, double p, int h, int n, int cY, int c, int nR) {
 		super(b, p, h, n , cY, c);
 		this.numRot = nR;
 	}
-	//GETTER & SETTER
+	// Getters and setters
 	public int getNumRot() {
 		return numRot;
 	}
 	public void setNumRot(int numRot) {
 		this.numRot = numRot;
 	}
-	//TO STRING METHOD
+	// toString method
 	@Override
 	public String toString() {
-		return "\n**Helicopter**\nBrand: " + brand + "\nPrice: $" + price + "\nHorsepower: " + horsepower 
+		return "\n**Multirotor**\nBrand: " + brand + "\nPrice: $" + price + "\nHorsepower: " + horsepower 
 				+ "\nNumber of Cylinders: " + numCyl + "\nCreation Year: " + cYear 
 				+ "\nPassenger Capacity: " + cap + "\nNumber of Rotors: " + numRot;
 	}
-	//EQUALS METHOD
+	// equals method
 	@Override
 	public boolean equals(Object obj) {
 		if (!super.equals(obj))
@@ -47,5 +51,10 @@ public class Multirotor extends Helicopter {
 		Multirotor other = (Multirotor) obj;
 		return numRot == other.numRot;
 	}	
+	// copy method
+	@Override
+	public Multirotor copy() {
+		return new Multirotor(this); // calls copy Multirotor constructor
+	}
 }
 
